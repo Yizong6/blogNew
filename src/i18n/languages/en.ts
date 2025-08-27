f964d3b08f6e3efea4fa46647e3e4234cac38f9c
y617012389 <y617012389@gmail.com>
2025-08-28 01:15:41 +0800

    布局调整，新增隐私政策和服务商

--- src/i18n/languages/en.ts
+++ src/i18n/languages/en.ts
@@ -3,7 +3,7 @@ import type { Translation } from "../translation";
 
 export const en: Translation = {
 	[Key.home]: "Home",
-	[Key.about]: "About",
+	[Key.about]: "About&Privacy",
 	[Key.archive]: "Archive",
 	[Key.search]: "Search",
 
